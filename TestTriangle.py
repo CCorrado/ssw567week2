7973ff6403c3640d24413fe1eb1986d0ce584430
Christopher Corrado <ccorrad@its.jnj.com>
2018-09-08 10:02:43 -0400

    Add Additional Test Cases

--- TestTriangle.py
+++ TestTriangle.py
@@ -19,13 +19,19 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self):
-        self.assertEqual(classifyTriangle(3, 4, 5), 'Right', '3,4,5 is a Right triangle')
+        self.assertEqual('Right', classifyTriangle(3, 4, 5), '3,4,5 is a Right triangle')
 
     def testRightTriangleB(self):
-        self.assertEqual(classifyTriangle(5, 3, 4), 'Right', '5,3,4 is a Right triangle')
+        self.assertEqual('Right', classifyTriangle(5, 3, 4), '5,3,4 is a Right triangle')
 
     def testEquilateralTriangles(self):
-        self.assertEqual(classifyTriangle(1, 1, 1), 'Equilateral', '1,1,1 should be equilateral')
+        self.assertEqual('Equilateral', classifyTriangle(1, 1, 1), '1,1,1 should be equilateral')
+
+    def testScaleneTriangle(self):
+        self.assertEqual('Scalene', classifyTriangle(14, 3, 42), '14,3,2 is a Scalene triangle')
+
+    def testIsoceles(self):
+        self.assertEqual('Isoceles', classifyTriangle(3, 3, 2), '3,3,2 is a Isoceles triangle')
 
 
 if __name__ == '__main__':
